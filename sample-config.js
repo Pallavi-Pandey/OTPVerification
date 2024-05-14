7bd0f8586e5ed70a9b9d2bbd82bd56366d52348b
Pallavi-Pandey <pallavipandey181@gmail.com>
2024-05-14 19:38:10 +0530

    added otp firebase"

---
+++ sample-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCHtOl-zFFpcV2z0dasdjsadtrFyl4qfkpdE",
+    authDomain: "pvedasdrification-ccca5.firebaseapp.com",
+    projectId: "oadbiasdfication-ccca5",
+    storageBucket: "oadbiasdfication-ccca5.appspot.com",
+    messagingSenderId: "271223213",
+    appId: "1:212321372704989853:web:9ed323132139c8468ead2d8274"
+};
